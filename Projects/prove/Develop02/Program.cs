f2620f1ca19115e8469201f81c73fc08e9322a7f
Iván Monte <ivanmonte07.93@gmail.com>
2023-02-04 08:48:40 -0300

    changes added

--- Projects/prove/Develop02/Program.cs
+++ Projects/prove/Develop02/Program.cs
@@ -4,12 +4,74 @@ using System.IO;
 
 class Journal
 {
-   static void Main(string[] args)
+    private List<Tuple<string, string, DateTime>> journal = new List<Tuple<string, string, DateTime>>();
+    private Random rnd = new Random();
+    private string[] questions = new string[] { "How do you felt today?", "What are the most significant things that you have learned today?", "Write some scripture you have liked in your study",
+    "Write your thoughts about your life in the Gospel" };
+
+    public void Write()
     {
-        List<Tuple<string, string, DateTime>> journal = new List<Tuple<string, string, DateTime>>();
-        Random rnd = new Random();
-        string[] questions = new string[] { "How do you felt today?", "What are the most significant things that you have learned today?", "Write some scripture you have liked in your study",
-        "Write your thoughts about your life in the Gospel" };
+        int index = rnd.Next(questions.Length);
+        Console.WriteLine("You selected 'Write'");
+        Console.WriteLine(questions[index]);
+        string answer = Console.ReadLine();
+        journal.Add(Tuple.Create(questions[index], answer, DateTime.Now));
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("You selected 'Display'");
+        foreach (var entry in journal)
+        {
+            Console.WriteLine("Question: " + entry.Item1);
+            Console.WriteLine("Answer: " + entry.Item2);
+            Console.WriteLine("Date/Time: " + entry.Item3);
+        }
+    }
+
+    public void Load()
+    {
+        Console.WriteLine("You selected 'Load'");
+        Console.WriteLine("Enter the name of the file to open");
+        string openFile = Console.ReadLine();
+        if (File.Exists(openFile))
+        {
+            using(StreamReader reader = new StreamReader(openFile))
+            {
+                string json = reader.ReadToEnd();
+                journal = System.Text.Json.JsonSerializer.Deserialize<List<Tuple<string, string, DateTime>>>(json);
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found. Please try again.");
+        }
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("You selected 'Save'");
+        Console.WriteLine("Enter the name of the file to save");
+        string saveFile = Console.ReadLine();
+        using (StreamWriter writer = new StreamWriter(saveFile))
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(journal);
+            writer.Write(json);
+        }
+    }
+
+    public void Quit()
+    {
+        Console.WriteLine("You selected 'Quit'");
+        Environment.Exit(0);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Journal journal = new Journal();
 
         while (true)
         {
@@ -20,61 +82,28 @@ class Journal
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
 
-            string userInput = Console.ReadLine();
-
-            switch (userInput)
+            string option = Console.ReadLine();
+            switch (option)
             {
                 case "1":
-                    int index = rnd.Next(questions.Length);
-                    Console.WriteLine("You selected 'Write'");
-                    Console.WriteLine(questions[index]);
-                    string answer = Console.ReadLine();
-                    journal.Add(Tuple.Create(questions[index], answer, DateTime.Now));
+                    journal.Write();
                     break;
                 case "2":
-                    Console.WriteLine("You selected 'Display'");
-                    foreach (var entry in journal)
-                    {
-                        Console.WriteLine("Question: " + entry.Item1);
-                        Console.WriteLine("Answer: " + entry.Item2);
-                        Console.WriteLine("Date/Time: " + entry.Item3);
-                    }
+                    journal.Display();
                     break;
-            case "3":
-                Console.WriteLine("You selected 'Load'");
-                Console.WriteLine("Enter the name of the file to open");
-                string openFile = Console.ReadLine();
-                if (File.Exists(openFile))
-                {
-                    using(StreamReader reader = new StreamReader(openFile))
-                    {
-                        string json = reader.ReadToEnd();
-                        journal = System.Text.Json.JsonSerializer.Deserialize<List<Tuple<string, string, DateTime>>>(json);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("File not found. Please try again.");
-                }
+                case "3":
+                    journal.Load();
                     break;
                 case "4":
-                    Console.WriteLine("You selected 'Save'");
-                Console.WriteLine("Enter the name of the file to save");
-                string saveFile = Console.ReadLine();
-                using (StreamWriter writer = new StreamWriter(saveFile))
-                {
-                    string json = System.Text.Json.JsonSerializer.Serialize(journal);
-                    writer.Write(json);
-                }
+                    journal.Save();
                     break;
                 case "5":
-                    Console.WriteLine("You selected 'Quit'");
-                    return;
-                default:
-                    Console.WriteLine("Invalid selection. Please try again.");
+                    journal.Quit();
                     break;
-                }
-            }            
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
         }
-    }    
-
+    }
+}
