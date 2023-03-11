44d732c77e23d5b3df9ea098b5c2626191a25487
Iván Monte <ivanmonte07.93@gmail.com>
2023-03-11 16:56:56 -0300

    Prepare/Learning05 Shapes added

---
+++ Projects/prepare/Learning05/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square("Red", 2.5);
+
+        Rectangle rectangle = new Rectangle("Blue", 2.5, 3);
+
+        Circle circle = new Circle("Yellow", 1);
+
+        List<Shape> listOfShapes = new List<Shape>();
+        listOfShapes.Add(square);
+        listOfShapes.Add(rectangle);
+        listOfShapes.Add(circle);
+
+        foreach(Shape shape in listOfShapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
+    }
+}
