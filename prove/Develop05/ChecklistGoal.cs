d809ca230d625f057341b61eaebbe2af29832523
Iván Monte <ivanmonte07.93@gmail.com>
2023-03-14 10:18:11 -0300

    Tracker Goals Program added

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,19 @@
+public class ChecklistGoal : Goal
+{
+    
+    public int GoalNeeded { get; set; }
+    public int GoalAccomplished { get; set; }
+    public int GoalBonus { get; set; }
+    public ChecklistGoal(string checkBox, string goalType, string name, string description, int points, bool status, int gAccomplished, int gNeeded, int bonus)
+        : base(checkBox, goalType, name, description, points, status)
+    {
+        GoalAccomplished = gAccomplished;
+        GoalNeeded = gNeeded;
+        GoalBonus = bonus;
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"{GoalCheckBox} - type: {GoalType} - {GoalName} - {GoalDescription} - {GoalPoints} points - {GoalStatus} ----- Currently completed: {GoalAccomplished}/{GoalNeeded} || bonus: {GoalBonus}");
+    }
+}
---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,14 @@
+public class EternalGoal : Goal
+{
+
+
+    public EternalGoal(string checkBox, string goalType, string name, string description, int points, bool status)
+        : base(checkBox, goalType, name, description, points, status)
+    {
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"{GoalCheckBox} - type: {GoalType} - {GoalName} - {GoalDescription} - {GoalPoints} points - {GoalStatus}");
+    }
+}
